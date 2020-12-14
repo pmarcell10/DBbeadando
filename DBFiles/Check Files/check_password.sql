447c1bb02ceca1d579e385527516483b4deb11bc
Marci <pmarcell50@gmail.com>
2020-12-14 18:18:50 +0100

    Adatbázis fájlok hozzáadása
    Check fájlok

---
+++ DBFiles/Check Files/check_password.sql
@@ -0,0 +1,21 @@
+﻿create or replace function sf_check_password
+(
+    p_password in admins.password%type
+)
+return int
+deterministic
+as
+    v_password_char char(1);
+    v_i int;
+begin
+    if p_password is null then
+        return 0;
+    end if;
+    
+	if length(trim(p_password)) < 6 then
+		return 0
+	end if;
+	  
+    return 1;
+end sf_check_password;
+        
---
+++ DBFiles/Check Files/check_uqid.sql
@@ -0,0 +1,36 @@
+﻿create or replace function sf_check_uqid
+(
+    p_uqid in items.uqid%type
+)
+return int
+deterministic
+as
+    v_uqid_char char(1);
+    v_i int;
+begin
+    if p_uqid is null then
+        return 0;
+    end if;
+    
+    if length(trim(p_uqid)) = 8 then
+        v_i := 1;
+        while v_i <= 4 loop
+            v_uqid_char := substr(p_uqid, v_i, 1);
+            if not (ascii('A') <= ascii(v_uqid_char) and ascii(v_uqid_char) <= ascii('Z')) then
+                return 0;
+            end if;
+            v_i := v_i + 1;
+        end loop;
+        
+        while v_i <= 8 loop
+            v_uqid_char := substr(p_uqid, v_i, 1);
+            if not (ascii('0') <= ascii(v_uqid_char) and ascii(v_uqid_char) <= ascii('9')) then
+                return 0;
+            end if;
+            v_i := v_i + 1;
+        end loop;
+        return 1;
+    end if;
+    return 0;
+end sf_check_uqid;
+        
